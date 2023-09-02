7441bd4073f32b1d11eaf2340e9a303fcb4c03c1
Horváth Balázs <hbali000@gmail.com>
2023-09-03 02:17:55 +0900

    Update string_extension.dart

--- lib/src/extension/string_extension.dart
+++ lib/src/extension/string_extension.dart
@@ -4,6 +4,5 @@ import '../localization/flutter_localization_translator.dart';
 
 extension StringExtension on String {
   String getString(BuildContext context) =>
-      FlutterLocalizationTranslator.of(context)?.getString(this) ??
-      '$this not found';
+      FlutterLocalizationTranslator.instance.getString(this);
 }
